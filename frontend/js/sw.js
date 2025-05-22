c313f8b13c9eadb785c798037f828f3f5e29d445
Felipe Azeredo <felipe.azeredo89@gmail.com>
2025-05-21 23:18:11 -0300

    Atts Finais

--- frontend/js/sw.js
+++ frontend/js/sw.js
@@ -1,36 +1,44 @@
-const CACHE_NAME = 'estoque-app-v2';
+const CACHE_NAME = 'estoque-app-v3';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/css/index.css',
-  '/js/login.js',
-  '/Imagens/icon-192x192.png',
-  '/Imagens/icon-512x512.png'
+  './',
+  './index.html',
+  './css/index.css',
+  './js/login.js',
+  './Imagens/logoMarcas144x144.png',
+  './Imagens/logoMarcas192x192.png',
+  './Imagens/logoMarcas512x512.png'
 ];
 
-// Instalação
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-});
-
-// Estratégia Cache First
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(cache => {
+        console.log('Cache aberto');
+        return cache.addAll(urlsToCache);
+      })
+      .catch(err => console.error('Falha no cache:', err))
   );
 });
 
-// Atualização automática
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.filter(name => name !== CACHE_NAME)
-                 .map(name => caches.delete(name))
-      );
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request)
+          .then(fetchResponse => {
+            return caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, fetchResponse.clone());
+                return fetchResponse;
+              });
+          })
+          .catch(() => {
+            if (event.request.headers.get('accept').includes('text/html')) {
+              return caches.match('./index.html');
+            }
+          })
     })
   );
 });
