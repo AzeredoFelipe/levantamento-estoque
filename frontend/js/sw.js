c8d2b21b2011aa9cd2ba50dd3f33d0a877d5c944
Felipe Azeredo <felipe.azeredo89@gmail.com>
2025-05-21 22:32:17 -0300

    Melhoria: atualiza PWA com service worker e manifest

--- frontend/js/sw.js
+++ frontend/js/sw.js
@@ -1,15 +1,36 @@
-// Nome do cache
-const cacheName = 'app-cache-v1';
+const CACHE_NAME = 'estoque-app-v2';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/css/index.css',
+  '/js/login.js',
+  '/Imagens/icon-192x192.png',
+  '/Imagens/icon-512x512.png'
+];
 
-// Instalação do Service Worker
-self.addEventListener('install', (event) => {
-  console.log('Service Worker instalado');
+// Instalação
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
 });
 
-self.addEventListener('fetch', (event) => {
+// Estratégia Cache First
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+// Atualização automática
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+                 .map(name => caches.delete(name))
+      );
     })
   );
 });
