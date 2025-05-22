89de778b040a8123f9256a56707e9606b1344d5b
Felipe Azeredo <felipe.azeredo89@gmail.com>
2025-05-22 20:51:14 -0300

    Atualizacao tela inicial

--- frontend/js/sw.js
+++ frontend/js/sw.js
@@ -1,44 +1,24 @@
-const CACHE_NAME = 'estoque-app-v3';
+const CACHE_NAME = 'estoque-app-v4';
 const urlsToCache = [
   './',
   './index.html',
   './css/index.css',
   './js/login.js',
-  './Imagens/logoMarcas144x144.png',
-  './Imagens/logoMarcas192x192.png',
-  './Imagens/logoMarcas512x512.png'
+  '/Imagens/logoMarcas192x192.png',
+  '/manifest.json'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Cache aberto');
-        return cache.addAll(urlsToCache);
-      })
-      .catch(err => console.error('Falha no cache:', err))
+    .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request)
-          .then(fetchResponse => {
-            return caches.open(CACHE_NAME)
-              .then(cache => {
-                cache.put(event.request, fetchResponse.clone());
-                return fetchResponse;
-              });
-          })
-          .catch(() => {
-            if (event.request.headers.get('accept').includes('text/html')) {
-              return caches.match('./index.html');
-            }
-          })
-    })
+    .then(response => response || fetch(event.request))
   );
-});
+})
+        
